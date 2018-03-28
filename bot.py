33abeadec7aeb2f97ff1b1e41d846e2d9eacbdb0
BlackrockZero <37180321+BlackrockZero@users.noreply.github.com>
2018-03-28 13:47:40 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -465,14 +465,14 @@ async def help(ctx):
 @client.command(pass_context=True)
 async def ping(ctx):
     channel = ctx.message.channel
-    t1 = time.perf_counter()
-    await client.send_typing(channel)
-    t2 = time.perf_counter()
     author = ctx.message.author
     msg = discord.Embed(colour=0x210150, description= "")
     msg.title = ""
     msg.add_field(name=":satellite: ", value="`Yes, I am here. No need to ping me.`\n`Ping: {}ms`".format(round((t2-t1)*1000)))
     msg.set_footer(text=footer_text)
+    t1 = time.perf_counter()
+    await client.send_typing(channel)
+    t2 = time.perf_counter()
     await client.say(embed=msg)
     print("============================================================")
     print("}ping")
